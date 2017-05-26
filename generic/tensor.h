5595a54f727b32bbe001c6847db3f59336f705b6
Xiaohui Zhao <xiaohui.zhao@intel.com>
2017-05-27 03:21:06 +0800

    fix for build warning

--- generic/tensor.h
+++ generic/tensor.h
@@ -22,8 +22,10 @@ void TH_MKL_(copyBacktoTH)(THTensor * pTensor, THMKLTensor * src);
 void TH_MKL_(TH2MKL)(THMKLTensor * pTensor, THTensor * src);
 void TH_MKL_(MKL2TH)(THTensor * pTensor, THMKLTensor * src);
 void TH_MKL_(resize4d)(THMKLTensor *self, long size0, long size1, long size2, long size3);
+void TH_MKL_(resizeAs)(THMKLTensor *self, THMKLTensor *src);
 real* TH_MKL_(data)(THMKLTensor *self);
 int TH_MKL_(nElement)(const THMKLTensor *self);
+void TH_MKL_(free)(THMKLTensor *self);
 /*
 dnnError_t  TH_MKL_(createWorkspace)(THMKLTensor* pTensor);
 dnnError_t MKLDNN_(dnnLayoutCreate)(dnnLayout_t *pLayout, size_t dimension, const size_t size[], const size_t strides[]);
